dde5f16660a02e5184ff9cd65bf41663cc041b93
Mahmoud Badr <a-mabadr@microsoft.com>
2020-06-18 22:39:31 +0200

    adding puzzles

--- ProblemSolvingGamification/Assets/CabinetDoorInteraction.cs
+++ ProblemSolvingGamification/Assets/CabinetDoorInteraction.cs
@@ -5,26 +5,37 @@ using UnityEngine;
 public class CabinetDoorInteraction : MonoBehaviour {
 
 	Animator animator;
+    Collider cabinetCollider;
+    Camera player;
+    static bool isOpened = false;
 
 	// Use this for initialization
 	void Start () {
 		animator = gameObject.GetComponent<Animator>();
+        cabinetCollider = gameObject.GetComponent<Collider>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //Any condition we want
-        if (Input.GetKey(KeyCode.O))
+        player = Camera.main;
+        Vector3 playerPos = player.transform.position;
+
+        Debug.Log("inside:" + cabinetCollider.bounds.Contains(playerPos).ToString());
+        Debug.Log("opened:" + isOpened.ToString());
+        Debug.Log("pressed:" + Input.GetKey(KeyCode.E).ToString());
+        if (cabinetCollider.bounds.Contains(playerPos) && isOpened == false && Input.GetKey(KeyCode.E))
         {
+            Debug.Log("shouldExecuteAnimation");
             animator.SetTrigger("open");
             animator.ResetTrigger("close");
+            isOpened = true;
         }
 
-        if (Input.GetKey(KeyCode.C))
+        else if (cabinetCollider.bounds.Contains(playerPos) && isOpened == true && Input.GetKey(KeyCode.E))
         {
             animator.ResetTrigger("open");
             animator.SetTrigger("close");
+            isOpened = false;
         }
-
     }
 }
