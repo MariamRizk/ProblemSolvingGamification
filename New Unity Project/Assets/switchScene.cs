4b2a9b2e7cba6eb6c3bc48711f87775a2cbcddc9
Abdelrhman Magdy <a-abmag@microsoft.com>
2020-06-19 03:05:32 +0200

    Adding trash and closet puzzle scenes :sparkles:

---
+++ New Unity Project/Assets/switchScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Threading;
+
+public class switchScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("HEEEEEEEEEEEEEEEEEEEEEEEEEEEY");
+		// Thread.Sleep(20000);
+		SceneManager.LoadScene("Scene2");
+
+	}
+}
---
+++ ProblemSolvingGamification/Assets/TrashInitMsg.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrashInitMsg : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		InputField txtBoxInput = gameObject.GetComponent<InputField>();
+		Debug.Log(txtBoxInput);
+
+		txtBoxInput.text = "eyJ1c2VybmFtZSI6IkRlYWRIYWNrZXIiLCJwYXNzd29yZCI6InNoZXJsb2NrZWQifQ";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
